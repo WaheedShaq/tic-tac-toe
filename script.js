4ac49be52c3dfda53cd35ca047ad0c7154d934a8
WaheedShaq <waheedshaqra@gmail.com>
2023-10-21 06:28:29 +0300

    made a function that displays the 'O' on the board in relation to the cell ID

---
+++ script.js
@@ -0,0 +1,46 @@
+let origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+  document.querySelector('.endgame').style.display = 'none';
+  origBoard = Array.from(Array(9).keys());
+  console.log(origBoard);
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', turnClick, false);
+  }
+}
+
+function turnClick(cell) {
+  console.log(cell.target.id);
+  turn(cell.target.id, huPlayer);
+}
+
+function turn(cellId, player) {
+  origBoard[cellId] = player;
+  document.getElementById(cellId).innerText = player;
+  // console.log(cellId);
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) {
+    gameOver(gameWon);
+  }
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce;
+}
