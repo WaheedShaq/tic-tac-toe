b1c22dbee05acc2a0af45e8749d9bf1566b62d6f
WaheedShaq <waheedshaqra@gmail.com>
2023-10-21 07:11:41 +0300

    added the functionality to check the winning combination of the player and detect the winning combo from the array and display the winning combo with a blue background

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let origBoard;
+var origBoard;
 const huPlayer = 'O';
 const aiPlayer = 'X';
 const winCombos = [
@@ -18,29 +18,46 @@ startGame();
 function startGame() {
   document.querySelector('.endgame').style.display = 'none';
   origBoard = Array.from(Array(9).keys());
-  console.log(origBoard);
-  for (let i = 0; i < cells.length; i++) {
+  for (var i = 0; i < cells.length; i++) {
     cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
     cells[i].addEventListener('click', turnClick, false);
   }
 }
 
-function turnClick(cell) {
-  console.log(cell.target.id);
-  turn(cell.target.id, huPlayer);
+function turnClick(square) {
+  turn(square.target.id, huPlayer);
 }
 
-function turn(cellId, player) {
-  origBoard[cellId] = player;
-  document.getElementById(cellId).innerText = player;
-  // console.log(cellId);
+function turn(squareId, player) {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
   let gameWon = checkWin(origBoard, player);
-  if (gameWon) {
-    gameOver(gameWon);
-  }
+  if (gameWon) gameOver(gameWon);
 }
 
 function checkWin(board, player) {
-  let plays = board.reduce;
+  // a = accumolator
+  // e = element in the board array
+  // i = array (board) index
+
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == huPlayer ? 'blue' : 'red';
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false);
+  }
 }
